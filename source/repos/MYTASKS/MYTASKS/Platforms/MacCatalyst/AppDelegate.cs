38388470c33c5990ed7e47a86d504cfb1e276f9e
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-19 12:41:36 +0700

    Add Platforms/MacCatalyst/AppDelegate.cs

---
+++ source/repos/MYTASKS/MYTASKS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MYTASKS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
