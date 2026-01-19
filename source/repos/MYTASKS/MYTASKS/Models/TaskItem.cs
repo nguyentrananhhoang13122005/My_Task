08cbc1c9e509bbc4855f258cc86093b00d969898
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-19 12:23:59 +0700

    Add Models/TaskItem.cs

---
+++ source/repos/MYTASKS/MYTASKS/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MYTASKS.Models
+{
+    // Model phải kế thừa ObservableObject theo yêu cầu
+    public partial class TaskItem : ObservableObject
+    {
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private bool isCompleted;
+    }
+}
