7cd3cc90cc3c180712d6ab41b07b50e0f73fa0da
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-19 12:23:07 +0700

    Add MauiProgram.cs

---
+++ source/repos/MYTASKS/MYTASKS/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using MYTASKS.ViewModels;
+
+namespace MYTASKS
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddSingleton<TasksViewModel>();
+            builder.Services.AddSingleton<MainPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
