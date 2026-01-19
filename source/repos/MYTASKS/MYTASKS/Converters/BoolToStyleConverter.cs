351d6cdf4831a850dd76b89dec020321cbcc1a21
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-19 12:25:17 +0700

    Add Converters/BoolToStyleConverter.cs

---
+++ source/repos/MYTASKS/MYTASKS/Converters/BoolToStyleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace MYTASKS.Converters
+{
+    public class BoolToStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isCompleted && isCompleted)
+            {
+                return TextDecorations.Strikethrough;
+            }
+            return TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
