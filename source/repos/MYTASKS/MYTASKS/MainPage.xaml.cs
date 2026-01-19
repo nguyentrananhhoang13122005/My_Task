6e16b5df7528e6c5692000b2c573f396de6bdb07
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-19 12:22:23 +0700

    Add MainPage.xaml.cs

---
+++ source/repos/MYTASKS/MYTASKS/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MYTASKS.ViewModels;
+
+namespace MYTASKS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(TasksViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+    }
+}
